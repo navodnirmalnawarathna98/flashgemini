b5408a534a344a5501b387b9bc4fd4a216a11760
Navod Nirmal Nawarathna <71627414+navodnirmalnawarathna98@users.noreply.github.com>
2025-07-09 11:07:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 # $env:GEMINI_API_KEY="YOUR_API_KEY_HERE" (Windows PowerShell)
 # export GEMINI_API_KEY="YOUR_API_KEY_HERE" (Linux/macOS)
 try:
-    genai.configure(api_key="AIzaSyBzKUAFWfH9HAoRdPqLCejxIy-k4M2vjXw")
+    genai.configure(api_key="AIzaSyA99DkpkLrSGTJtX0uhelXsfD7Ev7ZKXQo")
 except KeyError:
     print("Key error.")
     exit(1) # Exit if API key is not found
